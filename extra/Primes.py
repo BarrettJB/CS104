8ebd7a08c47ab6fc68206979ec98de1af7109205
BarrettRnR <bryson.barrett@gmail.com>
2014-09-11 13:52:12 -0700

    Update Primes.py
    Made program more efficient. Still some room for improvement

--- extra/Primes.py
+++ extra/Primes.py
@@ -1,9 +1,10 @@
-import math
-for n in range(1,100):
-    prime = 0
-    for i in range(2,n):
-        if n%i == 0:
-            prime = 1
-    if prime == 1:
-        print n
-
+print(2) #saves some time in arithmetic for higher numbers
+for n in range (3,10000): #Sets the number to be tested
+        prime = True
+        i = 2
+        while (n > i): #needs or prime == false // find a way to test sqrt(n) > i
+                if (n % i == 0):
+                        prime = False
+                i = i + 1
+        if (prime == True): 
+                print (n)
